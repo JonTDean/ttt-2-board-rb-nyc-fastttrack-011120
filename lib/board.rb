97336419d4ecb131717bd08a75a650c367340d69
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 16:12:58 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,17 @@
 # Define the variable board below.
+
+
+row = [" ", " ", " "]
+
+#position Left 
+row[0]
+
+#position middle
+row[1]
+
+#position right
+row [2]
+
+#Move X to the position 2, middle
+
+row[1] = "X"
