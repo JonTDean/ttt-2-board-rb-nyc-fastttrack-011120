2a6b2f254c3b669adedfd43b921058fa4467a860
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 16:16:16 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,28 @@
 # Define the variable board below.
 
 
-row = [" ", " ", " "]
+topRow = [" ", " ", " "]
+middleRow = [" ", " ", " "]
+bottomRow = [" ", " ", " "]
 
 #position Left 
-row[0]
+topRow[0]
+middleRow[0]
+bottomRow[0]
 
 #position middle
-row[1]
+topRow[1]
+middleRow[1]
+bottomRow[1]
 
 #position right
-row [2]
+topRow[2]
+middleRow[2]
+bottomRow[2]
 
 #Move X to the position 2, middle
 
-row1 = row
-row2 = row
+middleRow[1] = "X"
+
+
+board = topRow, middleRow, bottomRow
